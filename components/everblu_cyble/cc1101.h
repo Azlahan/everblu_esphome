b1555a30364c9c1bd2c293b51be0ca531bf32243
Azlahan <masurier.jb@gmail.com>
2026-08-04 15:52:22 +0200

    renommage

--- components/everblu_cyble/cc1101.h
+++ components/everblu_cyble/cc1101.h
@@ -5,8 +5,6 @@
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
 
-#include "registers.h"
-
 namespace esphome {
     namespace everblu_cyble {
 
@@ -14,21 +12,16 @@ namespace esphome {
 
         class CC1101 : public Component {
         public:
-            // Configuration des broches
             void set_cs_pin(GPIOPin *pin);
             void set_gdo0_pin(GPIOPin *pin);
             void set_gdo2_pin(GPIOPin *pin);
 
-            // Initialisation
             void setup() override;
 
-            // Réception
             void receive();
 
-            // Lecture FIFO
             uint8_t read_fifo(uint8_t *buffer);
 
-            // Version du circuit
             uint8_t get_version();
 
         protected:
@@ -37,36 +30,7 @@ namespace esphome {
             GPIOPin *gdo2_pin_{nullptr};
         };
 
-// Instance globale utilisée par EverbluCyble
         extern CC1101 cc1101;
-
-// --------------------------------------------------------------------
-// Driver bas niveau (pour l'instant intégré au même composant)
-// --------------------------------------------------------------------
-
-        void cc1101_init();
-
-        void cc1101_reset();
-
-        void cc1101_write_reg(uint8_t addr, uint8_t value);
-
-        uint8_t cc1101_read_reg(uint8_t addr);
-
-        void cc1101_write_burst(uint8_t addr, uint8_t *buffer, uint8_t length);
-
-        void cc1101_read_burst(uint8_t addr, uint8_t *buffer, uint8_t length);
-
-        void cc1101_strobe(uint8_t command);
-
-        void cc1101_rx();
-
-        void cc1101_tx();
-
-        uint8_t cc1101_read_fifo(uint8_t *buffer);
-
-        void cc1101_write_fifo(uint8_t *buffer, uint8_t length);
-
-        uint8_t cc1101_get_version();
 
     }  // namespace everblu_cyble
 }  // namespace esphome
