76618e547df4653bfd9b02b4d9d53524e1345d02
Azlahan <masurier.jb@gmail.com>
2026-08-04 13:39:08 +0200

    Update sensor.py

--- components/everblu_cyble/sensor.py
+++ components/everblu_cyble/sensor.py
@@ -1,8 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
-from esphome.components import sensor
 from esphome.const import (
+    CONF_ID,
     DEVICE_CLASS_WATER,
     STATE_CLASS_TOTAL_INCREASING,
 )
