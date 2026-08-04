41625cca6eecbf4f935e961afa8b95474d57ab1e
Azlahan <masurier.jb@gmail.com>
2026-08-04 17:44:57 +0200

    mettre de côté le fichier cc1101_driver.cpp en rajoutant backup

--- components/everblu_cyble/cc1101.cpp
+++ components/everblu_cyble/cc1101.cpp
@@ -53,5 +53,104 @@ namespace esphome {
             return cc1101_get_version();
         }
 
+// --------------------------------------------------
+// Driver bas niveau CC1101 temporaire
+// --------------------------------------------------
+
+        static bool cc1101_initialized = false;
+
+
+        void cc1101_init()
+        {
+            ESP_LOGI(TAG, "Driver CC1101 init");
+
+            cc1101_reset();
+
+            cc1101_initialized = true;
+        }
+
+
+        void cc1101_rx()
+        {
+            if (!cc1101_initialized)
+                return;
+
+            ESP_LOGD(TAG, "CC1101 passe en RX");
+
+            cc1101_strobe(SRX);
+        }
+
+
+        uint8_t cc1101_read_fifo(uint8_t *buffer)
+        {
+            if (!cc1101_initialized)
+                return 0;
+
+            return 0;
+        }
+
+
+        uint8_t cc1101_get_version()
+        {
+            return cc1101_read_reg(VERSION);
+        }
+
+
+        void cc1101_reset()
+        {
+            ESP_LOGI(TAG, "Reset CC1101");
+
+            cc1101_strobe(SRES);
+        }
+
+
+        void cc1101_strobe(uint8_t command)
+        {
+            // SPI réel à venir
+        }
+
+
+        uint8_t cc1101_read_reg(uint8_t addr)
+        {
+            return 0;
+        }
+
+
+        void cc1101_write_reg(uint8_t addr, uint8_t value)
+        {
+        }
+
+
+        void cc1101_read_burst(
+                uint8_t addr,
+                uint8_t *buffer,
+                uint8_t length)
+        {
+        }
+
+
+        void cc1101_write_burst(
+                uint8_t addr,
+                uint8_t *buffer,
+                uint8_t length)
+        {
+        }
+
+
+        void cc1101_tx()
+        {
+            if (!cc1101_initialized)
+                return;
+
+            cc1101_strobe(STX);
+        }
+
+
+        void cc1101_write_fifo(
+                uint8_t *buffer,
+                uint8_t length)
+        {
+        }
+
     }  // namespace everblu_cyble
 }  // namespace esphome
