43f1490ef6bf52ed6091169c8b15123670de21c4
Azlahan <masurier.jb@gmail.com>
2026-08-04 11:29:50 +0200

    Update __init__.py

--- custom_components/everblu_cyble/__init__.py
+++ custom_components/everblu_cyble/__init__.py
@@ -1,1 +1,20 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 
+from esphome.components import spi
+
+from . import sensor
+
+
+DEPENDENCIES = [
+    "spi",
+    "sensor",
+]
+
+
+AUTO_LOAD = [
+    "sensor",
+]
+
+
+CONFIG_SCHEMA = sensor.CONFIG_SCHEMA
